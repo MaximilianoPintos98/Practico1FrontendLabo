d41a712f9a73776d4d54158d7a0199d23f43f4e1
Maximiliano Pintos <pintosmaxii98@gmail.com>
2021-04-01 16:57:46 -0300

    Trabajando con componentes y Routing 01/04/2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { HomeComponent } from './components/home/home.component';
+import { DetalleComponent } from './components/detalle/detalle.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    HomeComponent,
+    DetalleComponent,
+    BuscadorComponent
   ],
   imports: [
     BrowserModule,
